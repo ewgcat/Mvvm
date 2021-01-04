488a76fc6673324f2d5b5248338ae533bd72bc55
lishuaihua <850716183@qq.com>
2021-01-04 22:57:08 +0800

    动态权限获取

--- app/src/main/java/com/lishuaihua/mvvmframework/MainActivity.kt
+++ app/src/main/java/com/lishuaihua/mvvmframework/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.lishuaihua.mvvmframework
 
+import android.Manifest
 import android.os.Bundle
 import com.lishuaihua.baselib.base.BaseActivity
 import com.lishuaihua.baselib.ext.viewbind
 import com.lishuaihua.mvvmframework.databinding.ActivityMainBinding
+import com.lishuaihua.permissions.JackPermissions
+import com.lishuaihua.permissions.OnPermission
 
 class MainActivity : BaseActivity< MainViewModel>() {
     private val binding: ActivityMainBinding by viewbind()
@@ -12,7 +15,44 @@ class MainActivity : BaseActivity< MainViewModel>() {
         binding.let {
             it.tv.text =vm.text.value
         }
+        initPermissions(permissions)
 
+    }
+
+    internal var permissions = arrayOf(
+        Manifest.permission.READ_PHONE_STATE,
+        Manifest.permission.CALL_PHONE,
+        Manifest.permission.CAMERA,
+        Manifest.permission.READ_CALENDAR,
+        Manifest.permission.WRITE_CALENDAR,
+        Manifest.permission.READ_CONTACTS,
+        Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+        Manifest.permission.CHANGE_NETWORK_STATE,
+        Manifest.permission.ACCESS_NETWORK_STATE,
+        Manifest.permission.ACCESS_WIFI_STATE,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.READ_SMS
+    )
+    /**
+     * 运行时请求权限
+     */
+    private fun initPermissions(permissions: Array<String>) {
+
+        JackPermissions.with(this) // 申请安装包权限
+            .permission(permissions) // 申请多个权限
+            .request(object : OnPermission {
+                override fun onGranted(granted: List<String>, all: Boolean) {
+                    if (all) {
+
+                    } else {
+
+                    }
+                }
+
+                override fun onDenied(denied: List<String>, never: Boolean) {
+                }
+            })
     }
 
 }
