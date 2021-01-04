b80afb2239ec4eed4395d46ea8fddbacded1cedc
lishuaihua <850716183@qq.com>
2021-01-04 22:58:03 +0800

    kotlin

--- app/src/main/java/com/lishuaihua/mvvmframework/App.java
+++
@@ -1,6 +0,0 @@
-package com.lishuaihua.mvvmframework;
-
-import com.lishuaihua.baselib.base.BaseApplication;
-
-public class App extends BaseApplication {
-}
---
+++ app/src/main/java/com/lishuaihua/mvvmframework/App.kt
@@ -0,0 +1,5 @@
+package com.lishuaihua.mvvmframework
+
+import com.lishuaihua.baselib.base.BaseApplication
+
+class App : BaseApplication()
