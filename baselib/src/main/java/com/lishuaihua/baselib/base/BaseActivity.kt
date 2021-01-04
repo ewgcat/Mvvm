e141ea477bc106f57012c0c6af4c434a30c121cd
lishuaihua <850716183@qq.com>
2021-01-04 19:32:00 +0800

    适配屏幕

--- baselib/src/main/java/com/lishuaihua/baselib/base/BaseActivity.kt
+++ baselib/src/main/java/com/lishuaihua/baselib/base/BaseActivity.kt
@@ -1,7 +1,13 @@
 package com.lishuaihua.baselib.base
 
+import android.app.Activity
+import android.app.Application
+import android.content.ComponentCallbacks
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.annotation.LayoutRes
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModel
@@ -28,9 +34,43 @@ open abstract class BaseActivity< U : BaseViewModel>() : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         resId = getLayoutResId()
         require(resId > 0) { "The subclass must provider a valid layout resources id." }
+        setCustomDensity(BaseApplication.instance,this)
         setContentView(resId)
         vm = createViewModel()
         doCreateView(savedInstanceState)
+    }
+    private var aNoncompatDensity = 0f
+    private var aNoncompatScaledDensity = 0f
+
+    private  fun setCustomDensity(application: Application, activity: Activity) {
+        val appDisplayMetrics: DisplayMetrics = application.getResources().getDisplayMetrics()
+        if (aNoncompatDensity == 0f) {
+            aNoncompatDensity = appDisplayMetrics.density
+            aNoncompatScaledDensity = appDisplayMetrics.scaledDensity
+            application.registerComponentCallbacks(object : ComponentCallbacks {
+                override fun onConfigurationChanged(@NonNull newConfig: Configuration) {
+                    if (newConfig.fontScale > 0) {
+                        aNoncompatScaledDensity = application.getResources().getDisplayMetrics().scaledDensity
+                    }
+                }
+
+                override fun onLowMemory() {}
+            })
+        }
+        val targetDensity = (appDisplayMetrics.widthPixels / 360).toFloat()
+        val targetScaledDensity = targetDensity * (aNoncompatScaledDensity / aNoncompatDensity)
+        val targetDensityDpi = (targetDensity * 160).toInt()
+
+        //设置application的Density
+        appDisplayMetrics.density = targetDensity
+        appDisplayMetrics.scaledDensity = targetScaledDensity
+        appDisplayMetrics.densityDpi = targetDensityDpi
+
+        //设置activity的Density
+        val activityDisplayMetrics = activity.resources.displayMetrics
+        activityDisplayMetrics.density = targetDensity
+        activityDisplayMetrics.scaledDensity = targetScaledDensity
+        activityDisplayMetrics.densityDpi = targetDensityDpi
     }
 
     protected abstract fun doCreateView(savedInstanceState: Bundle?)
--- baselib/src/main/java/com/lishuaihua/baselib/base/BaseApplication.kt
+++ baselib/src/main/java/com/lishuaihua/baselib/base/BaseApplication.kt
@@ -5,5 +5,14 @@ import android.app.Application
 open class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        instance = this
+    }
+
+    companion object {
+        private val TAG = BaseApplication::class.java.simpleName
+
+        @get:Synchronized
+        lateinit var instance: BaseApplication
+
     }
 }
