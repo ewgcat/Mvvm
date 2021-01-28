8e67fdcaee3603b443939b31465fcdfbff1451e5
lishuaihua <850716183@qq.com>
2021-01-28 17:16:40 +0800

    删除无用代码

--- workmanagerlib/src/main/java/com/lishuaihua/workmanagerlib/WorkManagerActivity.kt
+++ workmanagerlib/src/main/java/com/lishuaihua/workmanagerlib/WorkManagerActivity.kt
@@ -39,8 +39,22 @@ class WorkManagerActivity : BaseActivity<BaseViewModel>(), OnSharedPreferenceCha
      * @param view
      */
     fun testBackgroundWork1(view: View?) {
-        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MainWorker1::class.java).build()
-        WorkManager.getInstance(this).enqueue(oneTimeWorkRequest)
+
+        // 请求对象
+        val request = OneTimeWorkRequest.Builder(MainWorker3::class.java)
+            .build()
+
+        //  初始化环境  初始化工作源码
+        /**
+         * 这是第二次执行getInstance  ---  最终返回：WorkManagerImpl
+         *
+         * APK清单文件里面（第一次）执行
+         * 1.初始化 数据库 ROOM 来保存你的任务 （持久性保存的） 手机重新，APP被杀掉 没关系 一定执行
+         * 2.初始化 埋下伏笔  new GreedyScheduler
+         * 3.初始化 配置信息 configuration  （执行的信息，线程池任务，...）
+         */
+        WorkManager.getInstance(this) // 这是第二次执行，        APK清单文件里面（第一次）执行  面试官
+            .enqueue(request)
     }
 
     /**
@@ -207,29 +221,6 @@ class WorkManagerActivity : BaseActivity<BaseViewModel>(), OnSharedPreferenceCha
         updateToUI()
     }
 
-    /**
-     * 分析源码
-     * 测试后台任务 6
-     * @param view
-     */
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    fun codeStudy(view: View?) {
 
-        // 请求对象
-        val request = OneTimeWorkRequest.Builder(MainWorker3::class.java)
-            .build()
-
-        //  初始化环境  初始化工作源码
-        /**
-         * 这是第二次执行getInstance  ---  最终返回：WorkManagerImpl
-         *
-         * APK清单文件里面（第一次）执行  面试官
-         * 1.初始化 数据库 ROOM 来保存你的任务 （持久性保存的） 手机重新，APP被杀掉 没关系 一定执行
-         * 2.初始化 埋下伏笔  new GreedyScheduler
-         * 3.初始化 配置信息 configuration  （执行的信息，线程池任务，...）
-         */
-        WorkManager.getInstance(this) // 这是第二次执行，        APK清单文件里面（第一次）执行  面试官
-            .enqueue(request)
-    }
 
 }
