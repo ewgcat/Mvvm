b306a1c9affe600b5619f715d91a6d137f366869
lishuaihua <850716183@qq.com>
2021-02-02 00:27:42 +0800

    JAVA 转Kotlin

---
+++ home_module/src/main/java/com/lishuaihua/home_module/CommandLogin.kt
@@ -0,0 +1,56 @@
+package com.lishuaihua.home_module
+
+import android.os.RemoteException
+import android.util.Log
+import com.google.auto.service.AutoService
+import com.google.gson.Gson
+import com.lishuaihua.baselib.autoservice.IUserCenterService
+import com.lishuaihua.baselib.autoservice.ServiceLoaderHelper
+import com.lishuaihua.baselib.eventbus.LoginEvent
+import com.lishuaihua.webview.ICallbackFromMainprocessToWebViewProcessInterface
+import com.lishuaihua.webview.command.Command
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import java.util.*
+
+@AutoService(Command::class)
+class CommandLogin : Command {
+    var iUserCenterService = ServiceLoaderHelper.load(
+        IUserCenterService::class.java
+    )
+    var callback: ICallbackFromMainprocessToWebViewProcessInterface? = null
+    var callbacknameFromNativeJs: String? = null
+    override fun name(): String {
+        return "login"
+    }
+
+    override fun execute(
+        parameters: Map<*, *>?,
+        callback: ICallbackFromMainprocessToWebViewProcessInterface
+    ) {
+        Log.d("CommandLogin", Gson().toJson(parameters))
+        if (iUserCenterService != null && !iUserCenterService!!.isLogined) {
+            iUserCenterService!!.login()
+            this.callback = callback
+            callbacknameFromNativeJs = parameters?.get("callbackname").toString()
+        }
+    }
+
+    @Subscribe
+    fun onMessageEvent(event: LoginEvent) {
+        Log.d("CommandLogin", event.userName)
+        val map: HashMap<String, String> = HashMap<String, String>()
+        map.put("accountName",event.userName)
+        if (callback != null) {
+            try {
+                callback!!.onResult(callbacknameFromNativeJs, Gson().toJson(map))
+            } catch (e: RemoteException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    init {
+        EventBus.getDefault().register(this)
+    }
+}
---
+++ home_module/src/main/java/com/lishuaihua/home_module/MainProcessTestActivity.kt
@@ -0,0 +1,11 @@
+package com.lishuaihua.home_module
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainProcessTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_process_test_activity)
+    }
+}
