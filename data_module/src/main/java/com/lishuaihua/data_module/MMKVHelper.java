935b407ad96a61b1be5caf7f7d5b7dfa534a1ea9
lishuaihua <850716183@qq.com>
2021-03-07 18:04:40 +0800

    新增data_module模块

---
+++ data_module/src/main/java/com/lishuaihua/data_module/MMKVHelper.java
@@ -0,0 +1,27 @@
+package com.lishuaihua.data_module;
+
+import android.content.Context;
+
+import com.tencent.mmkv.MMKV;
+
+public class MMKVHelper {
+
+    public static void init(Context context){
+        String dir = context.getFilesDir().getAbsolutePath() + "/mmkv_2";
+        MMKV.initialize(dir);
+    }
+
+    public static MMKV getdefaultMMKV(){
+        return  MMKV.defaultMMKV();
+    }
+
+    public static MMKV getSingleMMKVWithID(String id){
+        return MMKV.mmkvWithID(id);
+    }
+
+    public static MMKV getMultiProcessMMKVWithID(String id){
+        return MMKV.mmkvWithID(id,MMKV.MULTI_PROCESS_MODE);
+    }
+
+
+ }
